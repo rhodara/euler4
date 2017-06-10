2d79e6cd8dcc18fe8a613c3e4461335ea9f57b6a
Roberta Hodara <hodara.roberta@gmail.com>
2017-06-09 22:16:37 -0700

    palindrome a function

--- euler4.py
+++ euler4.py
@@ -1,9 +1,20 @@
 '''Largest palindrome product
 Problem 4
 A palindromic number reads the same both ways.
-The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 √ó 99.
 Find the largest palindrome made from the product of two 3-digit numbers.'''
 
+def isPalindrome(numberWord):
+    end = len(numberWord)-1
+    i=0
+    isPal=1
+    while (i<=len(numberWord)/2) and (isPal==1):
+        if (numberWord[i]!=numberWord[end]):
+            isPal = 0
+        i+=1
+        end-=1
+    return isPal
+	
 
 def euler4():
     start=999
@@ -13,17 +24,9 @@ def euler4():
         for n in range(j,final,-1):
             product= n * j
             numberWord=str(product)
-            end = len(numberWord)-1
-            i=0
-            while i<=len(numberWord)/2 and (numberWord[i]==numberWord[end]):
-                i+=1
-                end-=1
-                if i-1 == (len(numberWord))/2:
-                    max = int(numberWord)
-                    return max
-
-
-
+            if (isPalindrome(numberWord)):
+                nWord = int(numberWord)
+                if nWord >max:
+                    max = nWord
+    return max
 print(euler4())
-
-print ("done")
